0af55a0762afda58bb5239114785faaa0b0b0f42
Marco De Lucchi <marcodelucchi27@gmail.com>
2019-05-16 12:15:19 +0200

    Parse languages

--- src/app/children/main/main.component.ts
+++ src/app/children/main/main.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
+import {languages} from '../../../assets/languages';
 import {HttpService} from '../../core/service/http.service';
+import {MostActiveCountry} from './component/chart-most-active-countries/chart-most-active-countries.component';
 
 @Component({
   selector: 'app-main',
@@ -8,14 +10,20 @@ import {HttpService} from '../../core/service/http.service';
 })
 export class MainComponent implements OnInit {
 
+  languages = languages;
+
   mostActiveContriesData = [];
 
   constructor(private httpService: HttpService) {
   }
 
   ngOnInit() {
-    this.httpService.get('assets/exported_tweets_countries.json').subscribe(res => {
-      this.mostActiveContriesData = res;
+    this.httpService.get('assets/exported_tweets_countries.json').subscribe((res: MostActiveCountry[]) => {
+      this.mostActiveContriesData = res.map(i => {
+        const name = this.languages.find(language => language.code === i.country.toLowerCase()).name;
+        i.country = name ? name : i.country;
+        return i;
+      }).sort((a, b) => a.country > b.country ? 1 : -1);
     });
   }
 
